8f64625e39be9d688d8ca7aa2a05771e95c5f557
Thalis-Silveira <thalissilbez7@gmail.com>
2021-05-31 02:36:46 -0300

    Adding first class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,10 @@
 package application;
 
-public class Program {
+import boardGame.Position;
 
+public class Program {
+	public static void main(String[] args) {
+		Position position = new Position(3, 5);
+		System.out.println(position);
+	}
 }
